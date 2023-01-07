acba8249ac7017e12da641f7e8f36a87fdf6ae91
Sk Arif <skarif2@gmail.com>
2023-01-08 03:41:15 +0600

    Initial deploy

---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import Tabs from 'components/Tabs';
+export default function Sidebar() {
+  return (
+    <div className="w-full h-full bg-teal-600">
+      <Tabs />
+      Sidebar
+    </div>
+  );
+}
---
+++ components/Signature.tsx
@@ -0,0 +1,3 @@
+export default function Signature() {
+  return <div className="h-full w-full bg-teal-100">Signature</div>;
+}
